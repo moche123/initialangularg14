529bac0305f87e4fc9a88f56b15dda518c46a31f
mlmania <mlmania908@gmail.com>
2024-01-31 20:14:46 -0500

    feature[project] > Implementing services

--- src/app/pages/characteres/characteres.component.ts
+++ src/app/pages/characteres/characteres.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { PagesService } from '../services/pages.service';
+import { ICharacter } from '../models/pages.model';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-characteres',
   templateUrl: './characteres.component.html',
-  styleUrls: ['./characteres.component.scss']
+  styleUrls: ['./characteres.component.scss'],
 })
 export class CharacteresComponent {
+  public characteres$ = new Observable<ICharacter[]>();
+  constructor(private _pagesService: PagesService, private _router: Router) {}
 
+  ngOnInit() {
+    this.characteres$ = this._pagesService.getCharacteres();
+  }
+  addFavorite(character: ICharacter) {
+    this._router.navigateByUrl('/pages/favorites');
+  }
 }
