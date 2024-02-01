ea505c7eb59e48b2e9e14a4a1bcc90093d738d04
mlmania <mlmania908@gmail.com>
2024-01-31 19:30:03 -0500

    feature[project]: Changes of components

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CharacteresComponent } from './characteres/characteres.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { PagesComponent } from './pages.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      {
+        path: 'characteres',
+        component: CharacteresComponent,
+      },
+      {
+        path: 'favorites',
+        component: FavoritesComponent,
+      },
+      { path: '', redirectTo: 'characteres', pathMatch: 'full' },
+      { path: '**', redirectTo: 'characteres', pathMatch: 'full' },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class PagesRoutingModule { }
+export class PagesRoutingModule {}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PagesRoutingModule } from './pages-routing.module';
-
+import { PagesComponent } from './pages.component';
+import { CharacteresComponent } from './characteres/characteres.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    PagesRoutingModule
-  ]
+  declarations: [PagesComponent, CharacteresComponent, FavoritesComponent],
+  imports: [CommonModule, PagesRoutingModule, SharedModule],
 })
-export class PagesModule { }
+export class PagesModule {}
